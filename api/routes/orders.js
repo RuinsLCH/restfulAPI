bce5227cb2cf8f0694710873dbe7707c31ca73cf
LCH <qoosky123@gmail.com>
2019-04-15 19:46:23 +0800

    check auth to protect some routes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const checkAuth = require('../middleware/check-auth');
 
 const Order = require('../models/order');
 const Product = require('../models/product');
@@ -32,7 +33,7 @@ router.get('/',(req, res, next)=>{
 		});
 });
 
-router.post('/',(req, res, next)=>{
+router.post('/', checkAuth, (req, res, next)=>{
 	Product.findById(req.body.productId)
 		.then(product =>{
 			if(!product){
@@ -71,7 +72,7 @@ router.post('/',(req, res, next)=>{
 
 });
 
-router.get('/:orderId',(req, res, next)=>{
+router.get('/:orderId', checkAuth,(req, res, next)=>{
 	Order.findById(req.params.orderId)
 		.select('quantity _id product')
 		.populate('product')
@@ -99,7 +100,7 @@ router.get('/:orderId',(req, res, next)=>{
 		});
 });
 
-router.delete('/:orderId',(req, res, next)=>{
+router.delete('/:orderId', checkAuth,(req, res, next)=>{
 	Order.remove({ _id: req.params.orderId })
 		.exec()
 		.then(result=>{
